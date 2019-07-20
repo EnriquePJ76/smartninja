da37d1bdbae0e3559101c9d994dae82396302d3f
Enrique PJ <enriquepj76@gmail.com>
2019-07-20 13:08:57 +0200

    Calculadora con while
    Se añade bucle while con opción q para salir

--- clase8/calculadora.py
+++ clase8/calculadora.py
@@ -1,30 +1,38 @@
 
 # bienvenida
 print ("\nBienvenido a mi primera calculadora de python.")
+
+
 print("Ahora introduce los datos")
 print()
 
-# En esta parte recoguemos los datos
-op = input("Que quieres hacer (+-*/): ")
-num1 = float(input("Introduce el primer número : "))
-num2 = float(input("Introduce el segundo número: "))
-resultado = None
+op = ""
+while op != "q":
+    # En esta parte recoguemos los datos
+    print ("Escribe q para salir.")
+    op = input("Que quieres hacer (+-*/q): ")
+    if op != "q":
+        num1 = float(input("Introduce el primer número : "))
+        num2 = float(input("Introduce el segundo número: "))
+    resultado = None
 
-# Aqui procesamos los datos
+    # Aqui procesamos los datos
 
-if op == "+":
-    resultado = num1 + num2
-elif op == "-":
-    resultado = num1 - num2
-elif op == "*":
-    resultado = num1 * num2
-elif op == "/":
-    resultado = num1 / num2
-else:
-    print ("No has metido una operacion correcta!!!")
-    exit()
+    if op == "+":
+        resultado = num1 + num2
+    elif op == "-":
+        resultado = num1 - num2
+    elif op == "*":
+        resultado = num1 * num2
+    elif op == "/":
+        resultado = num1 / num2
+    elif op == "q":
+        print ("Bye bye!!!")
+        exit()
+    else:
+        print ("No has metido una operacion correcta!!!")
 
 
-# En esta parte imprimimos los resultaods
-print ("\nEl resultado de la operación es:")
-print (str(num1) + " " + op + " " + str(num2) + " = " + str(resultado))
+    # En esta parte imprimimos los resultaods
+    print ("\nEl resultado de la operación es:")
+    print (str(num1) + " " + op + " " + str(num2) + " = " + str(resultado))
