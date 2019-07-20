0ef2ddbe30cfe9f2ff7d2c53a1f5606d02698bc3
Enrique PJ <enriquepj76@gmail.com>
2019-07-20 12:10:54 +0200

    Ejemplo de variables
    Trabajando con enteros, float y string.
    Input y print.

---
+++ clase8/pruebas.py
@@ -0,0 +1,19 @@
+
+
+
+x = float( input("Mete el valor de x: ") )
+y = float( input("Mete el valor de y: ") )
+suma = x + y
+
+texto = "En una variable - Resultado: "
+
+print(texto)
+print(suma)
+
+t0="Pepe"
+t1="es"
+t2="Programador"
+
+print (t0 + " " + t1 + " " + t2)
+
+print (str(x) + " + " + str(y) + " = "+ str(suma))
